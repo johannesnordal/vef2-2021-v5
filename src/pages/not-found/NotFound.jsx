9a4c3dbff8f2525d2497a3d249e2556e63c6f2b0
Jóhannes Nordal <johannesnordal88@gmail.com>
2021-03-27 19:34:29 +0000

    HAL

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -3,7 +3,7 @@ import s from './NotFound.module.scss';
 export function NotFound() {
   return (
     <div>
-      <h1 className={s.notFound}>404: Síða fannst ekki</h1>
+      <h2 className={s.notFound}>404: Síða fannst ekki</h2>
     </div>
   );
 }
